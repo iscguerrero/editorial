04896a44fb5545b611257eee335d69b9d3393fb0
isc.guerrero <isc.guerrero@outlook.com>
2017-10-31 15:38:00 -0600

    first commit

---
+++ application/controllers/Login.php
@@ -0,0 +1,135 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Login extends CI_Controller{
+
+	# Constructor del Controlador
+	function __construct(){
+		parent::__construct();
+		# Cargamos la base de datos por defecto
+			$this->load->database();
+		# Cargamos Helpers basicos
+			$this->load->helper(array('url','form'));
+		# Cargamos la libreria para la validacion de los formularios
+			$this->load->library(array('form_validation', 'session', 'encrypt'));
+		# Cargamos el modelo del catalogo de usuarios
+			$this->load->model('gl_cat_usuarios');
+	}
+
+	# Metodo para retornar la vista del login del sistema
+	public function Inicio($errors = null){
+		if ($this->session->userdata() && $this->session->userdata('logueado') == true) {
+			switch ($this->session->userdata('cve_perfil')) {
+				case '001':
+					redirect(site_url('Administracion/Clientes'));
+					break;
+				case '002':
+					redirect(site_url('Inicio/EstadisticasMensuales'));
+					break;
+				default:
+					redirect('/');
+					break;
+			}
+		} else{
+			$this->load->view('Login/inicio', $errors);
+		}
+	}
+
+	# Metodo para dar de alta un nuevo usuario
+	public function AltaUsuario() {
+		# Validamos los datos del formulario
+		$this->form_validation->set_rules('cve_usuario', 'Usuario', 'trim|required|min_length[4]|is_unique[gl_cat_usuarios.cve_usuario]', array(
+			'required' => 'El campo Usuario es requerido',
+			'min_length' => 'El campo Usuario debe contener al menos cuatro caracteres',
+			'is_unique' => 'Usuario registrado con anterioridad, intenta nuevamente',
+		));
+		$this->form_validation->set_rules('contrasenia', 'Contraseña', 'trim|required|min_length[6]', array(
+			'required' => 'El campo Contraseña es requerido',
+			'min_length' => 'El campo Contraseña debe contener al menos seis caracteres',
+		));
+		$this->form_validation->set_rules('confirmar_contrasenia', 'Confirmar Contraseña', 'trim|required|min_length[6]|matches[contrasenia]', array(
+			'required' => 'El campo Confirmar Contraseña es requerido',
+			'min_length' => 'El campo Confirmar Contraseña debe contener al menos seis caracteres',
+			'matches' => 'El campo Confirmar Contraseña debe coincidir con el campo Contraseña',
+		));
+		$this->form_validation->set_rules('cve_perfil', 'Perfil', 'required', array(
+			'required' => 'El campo Perfil es requerido'
+		));
+		$this->form_validation->set_rules('nombre', 'Nombre', 'trim|required', array(
+			'required' => 'El campo Nombre es requerido'
+		));
+		$this->form_validation->set_rules('correo', 'Correo', 'trim|required|valid_email|is_unique[gl_cat_usuarios.correo]', array(
+			'required' => 'El campo Correo es requerido',
+			'valid_email' => 'El correo registrado no cuenta con una estructura válida',
+			'is_unique' => 'El correo proporcionado ya se encuentra registrado en el sistema'
+		));
+		# Retornamos los errrores de validacion en caso de que estos se presente
+		if ($this->form_validation->run() === false) {
+			exit(json_encode(array('bandera'=>false, 'msj'=>'Las validaciones del formulario no se completaron, atiende:', 'error'=>validation_errors())));
+		} else {
+			# Guardamos los parametros de la peticion en variables locales
+			$cve_usuario = $this->input->post('cve_usuario');
+			$contrasenia = $this->input->post('contrasenia');
+			$cve_perfil = $this->input->post('cve_perfil');
+			$nombre = $this->input->post('nombre');
+			$correo = $this->input->post('correo');
+
+			if ($this->gl_cat_usuarios->altaUsuario($cve_usuario, $contrasenia, $cve_perfil, $nombre, $correo)) {
+				exit(json_encode(array('bandera'=>true, 'msj'=>'Registro creado con éxito')));
+			} else {
+				exit(json_encode(array('bandera'=>false, 'msj'=>'Se presento un error al crear el registro')));
+			}
+		}
+	}
+	# Metodo para loguear el usuario dentro del sistema
+	public function Acceder() {
+		# Validamos la cambinacion de usuario y contraseña de inicio de sesion
+		$this->form_validation->set_rules('cve_usuario', 'Usuario', 'required');
+		$this->form_validation->set_rules('contrasenia', 'Contraseña', 'required');
+		if ($this->form_validation->run() == false) {
+			exit(json_encode(array('bandera'=>false, 'msj'=>'Las validaciones del formulario no se completaron, atiende:', 'error'=>validation_errors())));
+		} else {
+			# Guardamos los parametros de la peticion en variables locales
+			$cve_usuario = $this->input->post('cve_usuario');
+			$contrasenia = $this->input->post('contrasenia');
+			# En caso de que la combinacion sea correcta
+			if ($this->gl_cat_usuarios->resolverLogin($cve_usuario, $contrasenia)) {
+				$usuario = $this->gl_cat_usuarios->obtenerUsuario($cve_usuario);
+				# Seteamos las variables de sesion
+				$nickname = explode(' ', $usuario->nombre);
+				$nickname = $nickname[0];
+				$sesion = array(
+					'cve_usuario' => $usuario->cve_usuario,
+					'cve_perfil' => $usuario->cve_perfil,
+					'nombre' => $usuario->nombre,
+					'correo' => $usuario->correo,
+					'nickname' => $nickname,
+					'logueado' => true
+				);
+				$this->session->set_userdata($sesion);
+				switch ($this->session->userdata('cve_perfil')) {
+					case '001':
+						redirect(site_url('Administracion/Clientes'));
+						break;
+					case '002':
+						redirect(site_url('Inicio/EstadisticasMensuales'));
+						break;
+					default:
+						redirect('/');
+						break;
+				}
+			} else {
+				redirect('/');
+			}
+		}
+	}
+	# Metodo para destruir los variables de sesion del usuario logueado
+	public function Salir() {
+		if ($this->session->userdata() && $this->session->userdata('logueado') == true) {
+			$sesion = array('logueado' => false);
+			$this->session->set_userdata($sesion);
+			redirect('/');
+		} else {
+			redirect('/');
+		}
+	}
+}
---
+++ application/views/master.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title><?php echo $this->e($title)?></title>
+		<link rel='icon' type='image/jpeg' href="<?php echo base_url('resources/images/site-ico-2.png')?>" />
+		<link rel="stylesheet" href="<?php echo base_url('resources/bootstrap/dist/css/bootstrap.min.css')?>">
+		<link rel="stylesheet" href="<?php echo base_url('resources/font-awesome/css/font-awesome.min.css')?>">
+		<link rel="stylesheet" href="<?php echo base_url('resources/nprogress/nprogress.css')?>">
+		<link rel="stylesheet" href="<?php echo base_url('resources/datetimepicker/build/css/bootstrap-datetimepicker.css')?>">
+		<link rel="stylesheet" href="<?php echo base_url('resources/toastr/toastr.min.css')?>">
+		<link rel="stylesheet" href="<?php echo base_url('resources/font-awesome/css/font-awesome.min.css')?>">
+		<link rel="stylesheet" href="<?php echo base_url('resources/switchery/dist/switchery.min.css')?>">
+		<link rel="stylesheet" href="<?php echo base_url('public/css/custom.min.css')?>">
+		<link rel="stylesheet" href="<?php echo base_url('public/css/comun.css')?>">
+		<?php echo $this->section('extra_style')?>
+	</head>
+	<body class="nav-md">
+
+		<div class="container body">
+			<div class="main_container">
+				<div class="col-md-3 left_col">
+					<div class="left_col scroll-view">
+						<div class="navbar nav_title" style="border: 0;">
+							<a href="<?php echo site_url('Inicio') ?>" class="site_title"><img src="<?php echo base_url('resources/images/site-ico.png')?>" class="img-responsive" alt="..."></a>
+						</div>
+						<div class="clearfix"></div>
+						<!-- menu profile quick info -->
+						<div class="profile clearfix">
+							<div class="profile_pic">
+								<img src="<?php echo base_url('resources/images/user.png')?>" alt="..." class="img-circle profile_img">
+							</div>
+							<div class="profile_info">
+								<span>Bienvenido,</span>
+								<h2><?php echo $_SESSION['nickname'] ?></h2>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+						<!-- /menu profile quick info -->
+						<br />
+						<!-- sidebar menu -->
+						<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+							<div class="menu_section">
+								<h3>Menu principal</h3>
+								<ul class="nav side-menu">
+									<li><a><i class="fa fa-line-chart"></i> Clientes <span class="fa fa-chevron-down"></span></a>
+										<ul class="nav child_menu">
+											<li><a href="<?php echo site_url('Inicio/Top5') ?>">Top 5</a></li>
+											<li><a href="<?php echo site_url('Inicio/estadisticasMensuales') ?>">Estadísticas Mensuales</a></li>
+										</ul>
+									</li>
+									<li><a><i class="fa fa-cogs"></i> Administración <span class="fa fa-chevron-down"></span></a>
+										<ul class="nav child_menu">
+											<li><a href="<?php echo site_url('Administracion/Clientes') ?>">Clientes</a></li>
+											<li><a href="<?php echo site_url('Administracion/Top5') ?>">Top 5</a></li>
+											<li><a href="<?php echo site_url('Administracion/Estadisticas') ?>">Estadísticas</a></li>
+										</ul>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<!-- /sidebar menu -->
+					</div>
+				</div>
+				<!-- top navigation -->
+				<div class="top_nav">
+					<div class="nav_menu">
+						<nav>
+							<div class="nav toggle">
+								<a id="menu_toggle"><i class="fa fa-bars"></i></a>
+							</div>
+							<ul class="nav navbar-nav navbar-right">
+								<li class="">
+									<a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+										<img src="<?php echo base_url('resources/images/user.png')?>" alt=""><?php echo $_SESSION['nickname'] ?>
+										<span class=" fa fa-angle-down"></span>
+									</a>
+									<ul class="dropdown-menu dropdown-usermenu pull-right">
+										<li><a href="javascript:;"> Perfil</a></li>
+										<li><a href="javascript:;">Ayuda</a></li>
+										<li><a href="<?php echo site_url('Login/Salir') ?>"><i class="fa fa-sign-out pull-right"></i> Salir</a></li>
+									</ul>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+				<!-- /top navigation -->
+
+				<!-- page content -->
+				<?php echo $this->section('page')?>
+				<!-- /page content -->
+
+				<!-- footer content -->
+				<footer>
+					<div class="pull-right">
+						rompemurosmx Copyright © 2017. All Right Reserved.
+					</div>
+					<div class="clearfix"></div>
+				</footer>
+				<!-- /footer content -->
+			</div>
+		</div>
+		<script src="<?php echo base_url('resources/jquery/dist/jquery.min.js')?>"></script>
+		<script src="<?php echo base_url('resources/bootstrap/dist/js/bootstrap.min.js')?>"></script>
+		<script src="<?php echo base_url('resources/fastclick/lib/fastclick.js')?>"></script>
+		<script src="<?php echo base_url('resources/nprogress/nprogress.js')?>"></script>
+		<script src="<?php echo base_url('resources/moment/min/moment.min.js')?>"></script>
+		<script src="<?php echo base_url('resources/moment/locale/es.js')?>"></script>
+		<script src="<?php echo base_url('resources/datetimepicker/build/js/bootstrap-datetimepicker.min.js')?>"></script>
+		<script src="<?php echo base_url('resources/Chart.js/dist/Chart.min.js')?>"></script>
+
+
+		<!-- jQuery Sparklines -->
+		<script src="<?php echo base_url('resources/jquery-sparkline/dist/jquery.sparkline.min.js')?>"></script>
+		<!-- Flot -->
+		<script src="<?php echo base_url('resources/Flot/jquery.flot.js')?>"></script>
+		<script src="<?php echo base_url('resources/Flot/jquery.flot.pie.js')?>"></script>
+		<script src="<?php echo base_url('resources/Flot/jquery.flot.time.js')?>"></script>
+		<script src="<?php echo base_url('resources/Flot/jquery.flot.stack.js')?>"></script>
+		<script src="<?php echo base_url('resources/Flot/jquery.flot.resize.js')?>"></script>
+		<!-- Flot plugins -->
+		<script src="<?php echo base_url('resources/flot.orderbars/js/jquery.flot.orderBars.js')?>"></script>
+		<script src="<?php echo base_url('resources/flot-spline/js/jquery.flot.spline.min.js')?>"></script>
+		<script src="<?php echo base_url('resources/flot.curvedlines/curvedLines.js')?>"></script>
+
+		<!-- DateJS -->
+		<script src="<?php echo base_url('resources/DateJS/build/date.js')?>"></script>
+
+		<script src="<?php echo base_url('resources/toastr/toastr.min.js')?>"></script>
+		<script src="<?php echo base_url('resources/switchery/dist/switchery.min.js')?>"></script>
+		<script src="<?php echo base_url('/public/js/custom.js')?>"></script>
+		<script src="<?php echo base_url('/public/js/comun.js')?>"></script>
+		<?php echo $this->section('extra_js')?>
+	</body>
+</html>
